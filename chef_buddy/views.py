4c4f7b8e2e195e0d042d170623fca26fdeb6a00f
Zack Cooper <zackjcooper@gmail.com>
2015-03-17 13:03:10 -0400

    many changes

--- chef_buddy/views.py
+++ chef_buddy/views.py
@@ -1,20 +1,15 @@
 import random
 import requests
-import pprint
-import timeit
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from chef_buddy.models import Recipe
+from chef_buddy.models import Recipe, UserFlavorCompound, RecipeFlavorCompound
 from chef_buddy_django.urls import RecipeSerializer
 #from chef_buddy.ingredient_fc_id import ingredient_to_fc_dict
 
 
 _app_id = '844ee8f7'
 _app_key = '9b846490c7c34c4f33e70564831f232b'
-pp = pprint.PrettyPrinter(indent=4)
-# Create your views here.
-
 
 
 @api_view(['GET'])
@@ -25,12 +20,22 @@ def get_random_recipe(request):
         serializer = RecipeSerializer(recipe)
         return Response(serializer.data)
 
-#@api_view(['GET'])
+@api_view(['GET'])
+def show_top_recipe(request):
+    """Manages actual top recipe request process"""
+    raw_yum_recipes = get_yummly_recipes() #grab recipes
+    fake_user_data = find_user_fc_ids(3) #grab user food compounds
+    rec_object, rec_food_compounds = rec_engine(raw_yum_recipes, fake_user_data) # pick top recipe for user
+    yield Response(rec_object) #returns recipe object to requester
+    store_recipe_fc(rec_object['id'], rec_food_compounds) #stores recipe_id to fc in database
+
+
 def get_yummly_recipes():
     request_amount = 40 #  change for amount of recipes returned from yummly
-    recipes = get_recipes(40)
+    recipes = get_recipes(request_amount)
     recipe_list = recipes['matches']
     return recipe_list
+
 
 def get_recipes(amount):
     random_start = random.randint(1, (321961 - amount))
@@ -44,50 +49,76 @@ def get_recipes(amount):
 
 
 def recipe_ingr_parse(recipe_list):
+    """recipe_list = raw list of recipes directly from yummly
+    takes in a list of recipes and returns a tupled list of recipe_id, ingredient"""
     return [(recipe['id'], ingredient) for recipe in recipe_list for ingredient in recipe['ingredients']]
 
 
-def fc_return(recipe_id, ingredient):
-    # will need to look in db table long term instead of dict.
-    return [(recipe_id, fc_id) for fc_id in ingredient_to_fc_dict[ingredient]]
-
-
-def recipe_fc_single_return(recipe_ingr_list):
-    recipe_fc_final = []
+def recipes_to_fc_id(recipe_list):
+    """ recipe_list = Raw Recipe input from yummly
+    Takes in a list of recipes and returns a tupled list of recipe_id to flavor compound id"""
+    recipe_ingr_list = recipe_ingr_parse(recipe_list)
+    recipe_fc_list = []
     for recipe_id, ingredient in recipe_ingr_list:
-        if ingredient in ingredient_to_fc_dict.keys():
-            recipe_fc_final.extend(fc_return(recipe_id, ingredient))
-    return recipe_fc_final
+        for fc_id in dbquery: # lookup ingredient in database and return list of fc matches
+            recipe_fc_list.append((recipe_id,fc_id))
+
+    return recipe_fc_list
 
 
-def recipe_list_fc_id(recipe_list):
-    recipe_ing = recipe_ingr_parse(recipe_list)
-    recipe_fc_id = {recipe['id']: [] for recipe in recipe_list}
-    for recipe_id, fc_id in recipe_fc_single_return(recipe_ing):
-        if recipe_id in recipe_fc_id:
-            recipe_fc_id[recipe_id].append(fc_id)
-    return recipe_fc_id
+def find_user_fc_ids(user_id):
+    """user_id = id of user in question
+    Looks up all the user's flavor compounds and associated scores. Returns them in a tupled list.
+    Also returns list of flavor compounds for that recipe"""
+    #flavor_compounds = UserFlavorCompound.objects.get(user_id=user_id)
+    # Turn them into dictionary {fc_id, score}
+    return {748: 1, 879: 4, 50: 2, 3: 8, 59: 1, 200: 2}
 
 
-def get_top_recipe_for_user(raw_recipes, user_fc_data):
-    recipe_id_fc_list = recipe_list_fc_id(raw_recipes)
-    recipe_to_fc_count = {recipe_id:0 for recipe_id in recipe_id_fc_list.keys()}
+def rec_engine(raw_recipes, user_fc_data):
+    """raw_recipes = list of raw recipes from yummly
+    user_fc_data = user to food compound dict
+    This function is the engine for picking a recipe given a user's food compounds. Returns 1 recipe object."""
+    recipe_id_fc_list = recipes_to_fc_id(raw_recipes)
 
-    for recipe_id, flavor_comp_id in recipe_id_fc_list.items():
-
-        for user_flav, user_score in user_fc_data:
-            if user_flav in flavor_comp_id:
-                recipe_to_fc_count[recipe_id] += user_score
-
-    return max(recipe_to_fc_count, key=recipe_to_fc_count.get)
+    match_dict = user_to_recipe_counter(recipe_id_fc_list, user_fc_data)
+    rec_id = max(match_dict, key=match_dict.get)
+    rec_id_fc_list = [fc_id for recipe_id, fc_id in recipe_id_fc_list if recipe_id == rec_id]
+    return recipe_id_to_object(rec_id, raw_recipes), rec_id_fc_list
 
 
-@api_view(['GET'])
-def show_top_recipe(request):
-    raw_yum_recipes = get_yummly_recipes()
-    fake_user_data = [(748, 1), (879, 4), (50, 2), (3,8), (59,1), (200,2)]
-    rec_id = get_top_recipe_for_user(raw_yum_recipes, fake_user_data)
-    for recipe in raw_yum_recipes:
-        if recipe['id'] == rec_id:
-            return Response(recipe)
-  
+def recipe_id_to_object(recipe_id, recipe_list):
+    """Looks recipe_id in recipe_list and returns recipe object"""
+    for recipe in recipe_list:
+        if recipe['id'] == recipe_id:
+            return recipe
+
+
+def user_to_recipe_counter(recipe_id_fc_list, user_fc_data):
+    """Takes in user's flavor compounds and recipe flavor compounds to produce a dict of how many times the flavor
+    compounds of the user appear in each recipe for the user. Each time a flavor compound appears, the score associated
+    with the user's fc will be added to the recipe"""
+
+    match_dict = {recipe_id:0 for recipe_id, fc_id in recipe_id_fc_list}
+    recipe_fc_count = match_dict.copy()
+    for recipe_id, fc_id in recipe_id_fc_list:
+        if fc_id in user_fc_data.keys():
+            match_dict[recipe_id] += user_fc_data[fc_id]
+            recipe_fc_count[recipe_id] += 1
+
+    normalize_ingr_count(match_dict, recipe_fc_count)
+    return match_dict
+
+def store_recipe_fc(recipe_id, flavor_compounds):
+    """recipe_id = id of the recipe needing to be housed
+    flavor_compounds = list of flavor compounds associated with recipe
+    This function is designed to take the above variables and store them in separate rows in the db"""
+    #for fc_id in flavor_compounds:
+        # Store as recipe_id, flavor_compound in db
+    return True
+
+def normalize_ingr_count(match_count_dict, recipe_to_fc_count_dict):
+    normalized = {}
+    for recipe_id, score in match_count_dict.items():
+        normalized[recipe_id] = score / recipe_to_fc_count_dict[recipe_id]
+    return normalized
