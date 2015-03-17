1c6ea3aba7801393ea2aad5d94111b5298922b32
Zack Cooper <zackjcooper@gmail.com>
2015-03-17 13:59:10 -0400

    added framework for logging recs

--- chef_buddy/views.py
+++ chef_buddy/views.py
@@ -26,8 +26,8 @@ def show_top_recipe(request):
     raw_yum_recipes = get_yummly_recipes() #grab recipes
     user_data = find_user_fc_ids(3) #grab user food compounds
     rec_object, rec_food_compounds = rec_engine(raw_yum_recipes, user_data) # pick top recipe for user
-    yield Response(rec_object) #returns recipe object to requester
-    store_recipe_fc(rec_object['id'], rec_food_compounds) #stores recipe_id to fc in database
+    #store_recipe_fc(rec_object['id'], rec_food_compounds) #stores recipe_id to fc in database
+    return Response(rec_object) #returns recipe object to requester
 
 
 def get_yummly_recipes():
@@ -60,11 +60,9 @@ def recipes_to_fc_id(recipe_list):
     recipe_ingr_list = recipe_ingr_parse(recipe_list)
     recipe_fc_list = []
     for recipe_id, ingredient in recipe_ingr_list:
-        flavors = IngredientFlavorCompounds.objects.filter(ingredient_id=ingredient)
-#First one dummy
+        flavors = IngredientFlavorCompound.objects.filter(ingredient_id=ingredient)
         for fc_id in flavors:
             recipe_fc_list.append((recipe_id,fc_id))
-
     return recipe_fc_list
 
 
@@ -73,8 +71,9 @@ def find_user_fc_ids(user_id):
     Looks up all the user's flavor compounds and associated scores. Returns them in a tupled list.
     Also returns list of flavor compounds for that recipe"""
 # This is another one dummy
-    flavor_compounds = UserFlavorCompound.objects.filter(user_id=user_id)
-    return {flavor.flavor_id: flavor.score for flavor in flavor_compounds}
+    # flavor_compounds = UserFlavorCompound.objects.filter(user_id=user_id)
+    # return {flavor.flavor_id: flavor.score for flavor in flavor_compounds}
+    return {397:3, 200:1, 1:1, 23:3, 45:1}
 
 
 def rec_engine(raw_recipes, user_fc_data):
@@ -86,6 +85,7 @@ def rec_engine(raw_recipes, user_fc_data):
     match_dict = user_to_recipe_counter(recipe_id_fc_list, user_fc_data)
     rec_id = max(match_dict, key=match_dict.get)
     rec_id_fc_list = [fc_id for recipe_id, fc_id in recipe_id_fc_list if recipe_id == rec_id]
+    log_recommendation()
     return recipe_id_to_object(rec_id, raw_recipes), rec_id_fc_list
 
 
@@ -97,9 +97,9 @@ def recipe_id_to_object(recipe_id, recipe_list):
 
 
 def user_to_recipe_counter(recipe_id_fc_list, user_fc_data):
-    """Takes in user's flavor compounds and recipe flavor compounds to produce a dict of how many times the flavor
-    compounds of the user appear in each recipe for the user. Each time a flavor compound appears, the score associated
-    with the user's fc will be added to the recipe"""
+    """Takes in user's flavor compounds and recipe flavor compounds to produce a dict of how
+    many times the flavor compounds of the user appear in each recipe for the user. Each time
+    a flavor compound appears, the score associated with the user's fc will be added to the recipe"""
 
     match_dict = {recipe_id:0 for recipe_id, fc_id in recipe_id_fc_list}
     recipe_fc_count = match_dict.copy()
@@ -121,7 +121,17 @@ def user_to_recipe_counter(recipe_id_fc_list, user_fc_data):
 #     return True
 
 def normalize_fc_count(match_count_dict, recipe_to_fc_count_dict):
+    print('raw count')
+    print(recipe_to_fc_count_dict)
     normalized = {}
     for recipe_id, score in match_count_dict.items():
-        normalized[recipe_id] = score / recipe_to_fc_count_dict[recipe_id]
+        normalized[recipe_id] = score
+        if normalized[recipe_id] > 0:
+            normalized[recipe_id] / recipe_to_fc_count_dict[recipe_id]
+    print('normalized')
+    print(normalized)
     return normalized
+
+def log_recommendation():
+    print('things')
+    return True
