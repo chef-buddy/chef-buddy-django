25fb996ab3e994201dfaf73e874189177df46569
Zack Cooper <zackjcooper@gmail.com>
2015-03-18 15:28:23 -0400

    adding remote db with amazon

--- chef_buddy_django/settings.py
+++ chef_buddy_django/settings.py
@@ -62,14 +62,14 @@ WSGI_APPLICATION = 'chef_buddy_django.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'chef_buddy',
-        'USER': 'johnmitsch',
-        'PASSWORD': 'johnmitsch',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-        }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'chef_buddy',
+       'USER': 'chefbuddyadmin',
+       'PASSWORD': 'chefbuddy123',
+       'HOST': 'cooper.cdhlqbkfdy0e.us-west-2.rds.amazonaws.com',
+       'PORT': '5432',
+       }
 }
 
 # if not os.environ.get("HOME") == '/Users/johnmitsch':
