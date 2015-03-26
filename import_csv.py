3c4b36547d825dea474d164b1bf11b4df7b69596
johnpmitsch <johnpmitsch@gmail.com>
2015-03-26 15:42:18 -0400

    new migrations

--- import_csv.py
+++ import_csv.py
@@ -4,14 +4,9 @@ from chef_buddy.models import IngredientFlavorCompound, UserFlavorCompound
 def seed_ingredients():
     with open("chef_buddy/raw_data/master_fc_ing.csv") as f:
         reader = csv.reader(f)
-        for row in reader:
-            print(row[0])
-            print(row[1])
-            created = IngredientFlavorCompound(
-                ingredient_id=row[0],
-                flavor_id=row[1],
-                )
-            created.save()
+        print('bulk creating...')
+        IngredientFlavorCompound.objects.bulk_create([IngredientFlavorCompound(ingredient_id=row[0],
+                                                                               flavor_id=row[1]) for row in reader])
 
 def seed_users():
     with open("chef_buddy/raw_data/user_fc.csv") as f:
