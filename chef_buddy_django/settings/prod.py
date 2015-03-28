6b259b3d140ab2a856f89cfbf787161c644c5d08
johnpmitsch <johnpmitsch@gmail.com>
2015-03-28 17:14:17 -0400

    changed database setting

--- chef_buddy_django/settings/prod.py
+++ chef_buddy_django/settings/prod.py
@@ -17,7 +17,7 @@ import os
 #
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['RDS_DB_NAME'],
         'USER': os.environ['RDS_USERNAME'],
         'PASSWORD': os.environ['RDS_PASSWORD'],
