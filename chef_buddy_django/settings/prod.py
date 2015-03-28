9cef4e907f5c48a45f00252eca898b53c4a565c5
johnpmitsch <johnpmitsch@gmail.com>
2015-03-28 15:57:46 -0400

    updated eb settings

--- chef_buddy_django/settings/prod.py
+++ chef_buddy_django/settings/prod.py
@@ -3,7 +3,7 @@ import os
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
+#
 # DATABASES = {
 #     'default': {
 #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
