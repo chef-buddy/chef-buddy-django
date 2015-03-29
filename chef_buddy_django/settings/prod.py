f0dc22c0a792c0ab9ea1d3aa7732ad9b2a90b3c7
johnpmitsch <johnpmitsch@gmail.com>
2015-03-29 10:20:42 -0400

    changed dbs for deploy

--- chef_buddy_django/settings/prod.py
+++ chef_buddy_django/settings/prod.py
@@ -4,27 +4,27 @@ import os
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 #
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'chef_buddy',
-       'USER': 'chefbuddyadmin',
-       'PASSWORD': os.environ['PASSWORD'],
-       'HOST': os.environ['HOST'],
-       'PORT': '5432',
-       }
-}
-#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ['RDS_DB_NAME'],
-#         'USER': os.environ['RDS_USERNAME'],
-#         'PASSWORD': os.environ['RDS_PASSWORD'],
-#         'HOST': os.environ['RDS_HOSTNAME'],
-#         'PORT': os.environ['RDS_PORT'],
-#     }
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'chef_buddy',
+#        'USER': 'chefbuddyadmin',
+#        'PASSWORD': os.environ['PASSWORD'],
+#        'HOST': os.environ['HOST'],
+#        'PORT': '5432',
+#        }
 # }
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
