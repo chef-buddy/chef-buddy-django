e8e05da326e11eda408688f8c5a38ccb28af0200
johnpmitsch <johnpmitsch@gmail.com>
2015-03-29 13:58:34 -0400

    added functional amazon rds database

--- chef_buddy_django/settings/prod.py
+++ chef_buddy_django/settings/prod.py
@@ -3,17 +3,28 @@ import os
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-#
 DATABASES = {
     'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'chef_buddy',
-       'USER': 'chefbuddyadmin',
-       'PASSWORD': os.environ['PASSWORD'],
-       'HOST': os.environ['HOST'],
-       'PORT': '5432',
-       }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tastebud',
+        'USER': 'tastebud',
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PASSWORD': 'tastebud',
+        'HOST': "johnmitsch.cq4v2ft4sf6d.us-east-1.rds.amazonaws.com",
+        'PORT': 5432,
+    }
 }
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'chef_buddy',
+#        'USER': 'chefbuddyadmin',
+#        'PASSWORD': os.environ['PASSWORD'],
+#        'HOST': os.environ['HOST'],
+#        'PORT': '5432',
+#        }
+# }
 #
 # DATABASES = {
 #     'default': {
@@ -25,13 +36,3 @@ DATABASES = {
 #         'PORT': os.environ['RDS_PORT'],
 #     }
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'tastebud',
-#         'PASSWORD': 'tastebud',
-#         'HOST': "aasw87elqyv9i7.cq4v2ft4sf6d.us-east-1.rds.amazonaws.com",
-#         'PORT': 5432,
-#     }
-# }
-#
