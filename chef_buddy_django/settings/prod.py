ce22bf68157970975940036a1ffddc3aea99bb02
johnpmitsch <johnpmitsch@gmail.com>
2015-03-31 14:00:01 -0400

    changed production settings

--- chef_buddy_django/settings/prod.py
+++ chef_buddy_django/settings/prod.py
@@ -5,11 +5,11 @@ import os
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tastebud',
         'USER': 'tastebud',
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': os.environ['HOST'],
-        'PORT': 5432,
+        'PASSWORD': 'tastebud',
+        'HOST': 'mysql.server',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
