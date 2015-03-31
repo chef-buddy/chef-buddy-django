864283e51a385e8ef9f7110e0d7fa33da0f6c56a
johnpmitsch <johnpmitsch@gmail.com>
2015-03-31 14:18:03 -0400

    updated heroku settings

--- chef_buddy_django/settings/prod.py
+++ chef_buddy_django/settings/prod.py
@@ -3,13 +3,24 @@ import os
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'tastebud',
+#         'USER': 'tastebud',
+#         'PASSWORD': 'tastebud',
+#         'HOST': 'mysql.server',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tastebud',
         'USER': 'tastebud',
-        'PASSWORD': 'tastebud',
-        'HOST': 'mysql.server',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'PASSWORD': os.environ['PASSWORD'],
+        'HOST': os.environ['HOST'],
+        'PORT': 5432,
     }
 }
